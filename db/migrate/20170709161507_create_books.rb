9dc0e1830293a950c030fda37b618d8f9fcb15c1
Philo Pöllmann <poellmann.philo@web.de>
2017-07-10 12:51:50 +0200

    updated migrations to match the new model design

--- db/migrate/20170709161507_create_books.rb
+++ db/migrate/20170709161507_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.integer :id
+      t.belongs_to :author, index:true
       t.integer :user_id
       t.datetime :published_at
       t.integer :noOfAuthors
