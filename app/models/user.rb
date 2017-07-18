632cffdd237c95a3096f9f81c1a4dc518147ddfd
Sumit Kapoor <Sumit.Kapoor@adesso.de>
2017-07-18 14:29:17 +0200

    user fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_many :books, dependent: :destroy, uniqueness: true
+  has_many :books, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
   has_secure_password
 end
