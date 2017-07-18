f7306310e1ab758138597d6d85acdecd39271048
Sumit Kapoor <Sumit.Kapoor@adesso.de>
2017-07-18 14:47:47 +0200

    added tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Philo", id: 1,
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = "foo" * 6
+    assert @user.valid?
+  end
 end
